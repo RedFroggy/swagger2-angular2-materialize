af5be02fb027d44362ba4396e44c5c5fcf889fd9
Michael Desigaud <michael.desigaud@thecosmocompany.com>
2016-11-23 17:48:55 +0100

    Default storage theme initialization

--- src/app/modules/app/themeable.component.ts
+++ src/app/modules/app/themeable.component.ts
@@ -7,6 +7,11 @@ import * as Config from '../../utils/env.config';
 import * as _ from 'lodash';
 
 export abstract class ThemeableComponent {
+    constructor() {
+        if (!localStorage.getItem(Config.LOCAL_STORAGE_THEME)) {
+            localStorage.setItem(Config.LOCAL_STORAGE_THEME, Config.THEMES[0].value);
+        }
+    }
     getThemeOption(option: any): string {
         let theme = this.getCurrentTheme();
         return theme[option];
